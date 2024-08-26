302ada67fefc2b906aa4d8c6fd9232bb509fc9c1
Noah Clark <noahammon00@gmail.com>
2024-08-26 17:34:05 -0600

    stripped message page of all features i was previously experimenting with

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,41 +1,15 @@
 import { StyleSheet, Button } from "react-native";
-import CustomBottomSheet from "@/components/util/CustomBottomSheet";
 import { ThemedView } from "@/components/ThemedView";
-import { BottomSheetModal } from "@gorhom/bottom-sheet"
-import { useRef } from "react";
 import { ThemedText } from "@/components/ThemedText";
 
 
 export default function messages() {
-  const bottomSheetRef = useRef<BottomSheetModal>(null)
-
-  const handleOpenPress = () => bottomSheetRef.current?.present()
 
   return (
-    <ThemedView style={styles.container}>
-      <Button title="open" onPress={handleOpenPress}></Button>
-      <CustomBottomSheet title="Share" ref={bottomSheetRef}>
-        <ThemedText style={styles.text}>Hello</ThemedText>
-      </CustomBottomSheet>
+    <ThemedView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 50,
-    backgroundColor: "light-grey",
-  },
-  contentContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-  indicator: {
-    backgroundColor: '#bebebe',
-    width: '20%'
-  },
-  text: {
-    textAlign: 'center'
-  }
 });
