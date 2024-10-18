003e9fcc4fcc41c83238bc4cae96a7c7d4867c5e
Noah Clark <noahammon00@gmail.com>
2024-10-18 12:20:57 -0600

    made the repost icon green when youve reposted a post

--- app/+html.tsx
+++ app/+html.tsx
@@ -15,7 +15,7 @@ export default function Root({ children }: PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-
+        <title>Kale & Noah's Social</title>
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
